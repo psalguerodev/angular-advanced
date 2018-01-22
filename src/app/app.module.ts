bd4bde5ddf65d92cb8312f49173b4626cdb789fe
Patrick Salguero Avalos <patrick.salguero.avalos@gmail.com>
2018-01-21 21:58:07 -0500

    Se agrega las referencias del Template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { NofoundComponent } from './nofound/nofound.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { ProgressComponent } from './pages/progress/progress.component';
 import { Grapth1Component } from './pages/grapth1/grapth1.component';
+import { NofoundComponent } from './shared/nofound/nofound.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { SidebardComponent } from './shared/sidebard/sidebard.component';
 import { BreadcrumComponent } from './shared/breadcrum/breadcrum.component';
